b5eff6bc4cc9498a96e6d3114b4be5970bde3ac7
Dmp-86 <danni_power69@hotmail.com>
2023-04-14 09:14:04 +0000

    Build logic for questions in javascript

--- game.js
+++ game.js
@@ -20,35 +20,77 @@ let questions = [
         answer: 2,
     },
     {
-        question: 'What element does 'O' represent on the periodic table?',
-        choice1: 'Helium',
-        choice2: 'Oxygen',
-        choice3: 'Cobalt',
-        choice4: 'Osmium',
-        answer: 2,
+        question: 'Proverbially, what is rubbed into the wound to make things worse?',
+        choice1: 'Salt',
+        choice2: 'Vinegar',
+        choice3: 'Ketchup',
+        choice4: 'Mustard',
+        answer: 1,
     },
     {
-        question: 'What element does 'O' represent on the periodic table?',
-        choice1: 'Helium',
-        choice2: 'Oxygen',
-        choice3: 'Cobalt',
-        choice4: 'Osmium',
-        answer: 2,
+        question: 'In fairy tales, which item is used to transport people through the air?',
+        choice1: 'Magic mattress',
+        choice2: 'Magic blanket',
+        choice3: 'Magic blanket',
+        choice4: 'Magic carpet',
+        answer: 4,
     },
     {
-        question: 'What element does 'O' represent on the periodic table?',
-        choice1: 'Helium',
-        choice2: 'Oxygen',
-        choice3: 'Cobalt',
-        choice4: 'Osmium',
-        answer: 2,
+        question: 'Which of these is a popular type of biscuit?',
+        choice1: 'Ingestive',
+        choice2: 'Suggestive',
+        choice3: 'Digestive',
+        choice4: 'Festive',
+        answer: 3,
     },
     {
-        question: 'What element does 'O' represent on the periodic table?',
-        choice1: 'Helium',
-        choice2: 'Oxygen',
-        choice3: 'Cobalt',
-        choice4: 'Osmium',
-        answer: 2,
+        question: ' A ‘goatee’ is a small type of what?',
+        choice1: 'Fork',
+        choice2: 'Goat',
+        choice3: 'Beard',
+        choice4: 'Cucumber',
+        answer: 3,
     },
 ]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 4
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestions()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionsCounter > MAX_QUESTIONS){
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+
+
+questionCounter++
+progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+
+const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+currentQuestion = availableQuestions[questionsIndex]
+question.innerText = currentQuestion.question
+
+choices.forEach(choice=> {
+    const number = choice.dataset['number']
+    choice.innerText = currentQuestion['choice' + number]
+})
+
+availableQuestions.splice(questionsIndex,1)
+
+acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e =>{
+        if(!acceptingAnswers) return
+    })
+})
